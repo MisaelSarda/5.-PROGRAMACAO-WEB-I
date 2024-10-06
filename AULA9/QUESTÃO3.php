511ceac57e4d4d42ef726f9f420cdf0368266f1d
Misael <misael.sarda@unidavi.edu.br>
2024-10-06 15:55:28 -0300

    COMMIT

--- AULA9/QUESTÃO3.php
+++ AULA9/QUESTÃO3.php
@@ -10,20 +10,19 @@
     <h1>Calcular a Área do Quadrado</h1>
 
     <form action="" method="POST">
-        <label for="side">Lado do quadrado (em metros):</label>
+        <label for="side">Lado do Quadrado (em metros):</label>
         <input type="number" step="0.01" name="side" id="side" required><br><br>
-
-        <input type="submit" value="Calcular Área">
+        <input type="submit" value="Calcular Área do Quadrado">
     </form>
 
     <?php
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $side = $_POST['side'];
-
         $area = $side * $side;
 
-        echo "<h2>A área do quadrado de lado " . number_format($side, 2, ',', '.') . " metros é " 
-             . number_format($area, 2, ',', '.') . " metros quadrados.</h2>";
+        echo "<h2>A Área do quadrado de lado " . number_format($side) . " metros é " 
+     . number_format($area) . " metros quadrados.</h2>";
+
     }
     ?>
 
