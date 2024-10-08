495a195f1c32a832f75bd96f787881b43272c2db
Misael <misael.sarda@unidavi.edu.br>
2024-10-08 20:47:36 -0300

    COMMIT

--- AULA9/QUESTÃO10.php
+++ AULA9/QUESTÃO10.php
@@ -1,46 +1,31 @@
 <?php
-
-class Arvore {
-    private $dados;
-
-    public function __construct($dados) {
-        $this->dados = $dados;
-    }
-
-    public function exibir($nivel = 0) {
-        $espaco = str_repeat('- ', $nivel);
-        
-        foreach ($this->dados as $chave => $valor) {
-            if (is_array($valor)) {
-                echo $espaco . $chave . "<br>";
-                // Cria uma nova instância de Arvore para os subarrays
-                (new Arvore($valor))->exibir($nivel + 1);
-            } else {
-                echo $espaco . $valor . "<br>";
-            }
+$pastas = array(
+    "bsn" => array(
+        "3a Fase" => array(
+            "desenvWeb",
+            "bancoDados 1",
+            "engSoft 1"
+        ),
+        "4a Fase" => array(
+            "Intro Web",
+            "bancoDados 2",
+            "engSoft 2"
+        )
+    )
+);
+function exibirPastas($array, $nivel = 0) {
+    $espaco = str_repeat('- ', $nivel);
+    
+    foreach ($array as $chave => $valor) {
+        if (is_array($valor)) {
+            echo $espaco . $chave . "<br>";
+            exibirPastas($valor, $nivel + 1); 
+        } else {
+            echo $espaco . $valor . "<br>";
         }
     }
 }
-
-// Definindo o array com os elementos da árvore
-$tree = [
-    'bsn' => [
-        '3a Fase' => [
-            'desenv Web',
-            'bancoDados',
-            'engSoft'
-        ],
-        '4a Fase' => [
-            'Intro Web',
-            'bancoDados 2',
-            'engSoft 2'
-        ]
-    ]
-];
-
-// Exibindo a árvore
-echo "<h1>Estrutura da Árvore</h1>";
-$minhaArvore = new Arvore($tree);
-$minhaArvore->exibir();
+echo "<h1>Estrutura de Árvore</h1>";
+exibirPastas($pastas);
 
 ?>
