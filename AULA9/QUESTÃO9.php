a0c9f3ab7aa50640f1cb4b2dac6ef63dd0e546f5
Misael <misael.sarda@unidavi.edu.br>
2024-10-08 20:41:19 -0300

    COMMIT

--- AULA9/QUESTÃO9.php
+++ AULA9/QUESTÃO9.php
@@ -11,10 +11,10 @@ $parcelas = [
     60 => 0.029,
 ];
 echo "<h1>Valor das Parcelas da Moto com Juros Compostos</h1>";
-echo "<p>Valor à vista da moto: R$ " . number_format($valor_moto, 2, ',', '.') . "</p>";
+echo "<p>Valor à vista da moto: <strong> R$ " . number_format($valor_moto, 2, ',', '.') . "</strong></p>";
 
 foreach ($parcelas as $num_parcelas => $taxa_juros) {
     $parcela = calcular_parcela($valor_moto, $taxa_juros, $num_parcelas);
-    echo "<p>Para $num_parcelas vezes com juros de " . ($taxa_juros * 100) . "% ao mês: R$ " . number_format($parcela, 2, ',', '.') . " por parcela</p>";
+    echo "<p>Para $num_parcelas vezes com juros de " . ($taxa_juros * 100) . "% ao mês: <strong> R$ " . number_format($parcela, 2, ',', '.') . "</strong> por parcela</p>";
 }
 ?>
