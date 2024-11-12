bf25606ff8ab9fb6e246a89c7669a96f0d391902
Misael <misael.sarda@unidavi.edu.br>
2024-11-12 20:26:10 -0300

    COMMIT

---
+++ AULA8/pratica5.php
@@ -0,0 +1,23 @@
+<?php 
+$disciplines = array(
+    "programação web =>",
+    "adm de sistema =>",
+    "eng software II =>",
+    "estrutura II =>",
+    "banco II = >"
+  );
+  
+  $professors = array(
+    "cleber",
+    "sandro",
+    "julia",
+    "bastos",
+    "marco"
+  );
+  
+  for ($i = 0; $i < 5; $i++) {
+    echo "Disciplina $disciplines[$i], professor $professors[$i].<br>";
+  }
+
+  Echo "<P>"
+  ?>
